3412974d7f4e2d13184be3ae84b3de5590e5a4d3
Drkeck <k3ckl3y.a@gmail.com>
2020-07-26 11:07:29 -0700

    added more to the about me section

--- src/componenets/About/index.js
+++ src/componenets/About/index.js
@@ -18,17 +18,31 @@ return(
             </Jumbotron>
             <div className="media container">
                 <img src={Img1} className="w-25 rounded" alt=".."/>
-                <div className="media-body">
+                <div className="media-body container">
                     <h2>Alexander Keckley</h2>
                     <h6>Coder, Editor, Friend</h6>
                     <p>My name is Alexander Keckley, and i am huge into Coding, Video editing, Live streaming, and meeting new people!
                         I am studying full stack development at UC Berkly Extension, there i have been making strides understanding how to build websites from front to back.
                          learning some of the industry standards and libraries and middleware that can help you achieve your vision.</p>
                     <p>Some of my skills are:</p>
-                    <ul className="text-left">
-                        <li> M.E.R.N. (Mango, Express, React, Node.js) </li>
-                        <li> Davinci Resolve/Fusion </li>
-                    </ul>
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <ul className="col-4">
+                                <h4>Coding:</h4>
+                                <li>Node.js</li>
+                                <li>SQL and MongoDB</li>
+                                <li>Css/HTML</li>
+                                <li>Api's and server</li>
+                                <li>Jquery</li>
+                            </ul>
+                            <ul className="col-4">
+                                <h4>Video Editing:</h4>
+                                <li> Davinci Resolve </li>
+                                <li> Fusion VFX </li>
+                                <li> Audio Mixing </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
