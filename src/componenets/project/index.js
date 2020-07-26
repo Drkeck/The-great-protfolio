0965674d07e876919bc753d427fa9d341a97fec6
Drkeck <k3ckl3y.a@gmail.com>
2020-07-26 10:30:27 -0700

    added a projects section and filled it with projects for the portfolio

--- src/componenets/project/index.js
+++ src/componenets/project/index.js
@@ -1,96 +1,77 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 
 function Portfolio() {
     const [projects] = useState([
         {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
+            name: "Jak-eats (api-keys N/A)",
+            description: "A collobrative project with food api's",
+            languages: "html, javascript, css, jquery",
+            github: "https://github.com/ktrnthsnr/JAK-eats",
+            site:"https://ktrnthsnr.github.io/JAK-eats/"
         },
         {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
-    
-        },
-        {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
+            name: "D&D character builder",
+            description: "Collabrotive effort to help D&D newcomers",
+            languages: "javascript, css, handlebars, node.js, SQL",
+            github: "https://github.com/bskutivan/character_builder",
+            site:"https://dnd-char-builder.herokuapp.com/"
     
         },
         {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
+            name: "Budget Tracker",
+            description: "Made a PWA budget tracker to get familiar with PWA's",
+            languages: "html, javascript, css",
+            github: "https://github.com/drkeck/Budget-Tracker",
+            site:"https://budget-trackor.herokuapp.com"
     
         },
         {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
+            name: "Little tech Blog",
+            description: "A Blog built to flex my SQL muscles",
+            languages: "html, javascript, css, SQL",
+            github: "https://github.com/drkeck/Little-tech-blog",
+            site:"https://little-tech-blog.herokuapp.com"
     
         },
         {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
+            name: "Highend Stickynote",
+            description: "This was how i learned how to create api's and what was needed.",
+            languages: "html, javascript, css",
+            github: "https://github.com/drkeck/Note-Taking",
+            site:"https://highendstickynote.herokuapp.com"
     
         },
         {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
+            name: "README generator",
+            description: "Made a high end README generator to learn node.js",
+            languages: "javascript, node.js",
+            github: "https://github.com/drkeck/Readme-generator",
+            site:"https://github.com/drkeck/Readme-generator"
     
-        },
-        {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
-    
-        },
-        {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
-    
-        },
-        {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
-    
-        },
-        {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
-    
-        },
-        {
-            name: "test",
-            description: "test description",
-            languages: "html, javascript, css"
-    
-        },
-    
+        }
     ])
     
 
     return(
-        <div>
-        {
-            projects.map((project, i) => (
-                <div className="card w-25 m-2" key={i}>
-                    <div className="card-body">
-                        <h5 className="card-title">{project.name}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{project.languages}</h6>
-                        <p className="card-text">{project.description}.</p>
-                        <a href="#" className="card-link">link</a>
-                    </div>
-                </div>
-            ))
-        }
+        <div className="container">
+            <h2 className="my-3">My Work:</h2>
+            <div className="row justify-content-center">
+                {
+                    projects.map((project, i) => (
+                        <div className="card m-2 col-5 shadow-sm bg-light
+                        " key={i}>
+                            <img src={require(`../../assets/projectImg/${i + 1}.jpg`)} className="card-img-top mt-5" alt="..." />
+                            <div className="card-body col">
+                                <h5 className="card-title">{project.name}</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">{project.languages}</h6>
+                                <p className="card-text">{project.description}.</p>
+                                <a href={project.github} className="card-link">GitHub</a>
+                                <a href={project.site} className="card-link">Site</a>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
